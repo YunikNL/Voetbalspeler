6e7a8e5ce273c488b5840db419ca81cee7449050
YunikNL <Freddyvandenbergh@live.nl>
2023-09-13 20:18:14 +0200

    Nederlands

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-
+# hier bepaal je de taal van de applicatie
 LANGUAGE_CODE = 'nl-nl'
 
 TIME_ZONE = 'UTC'
