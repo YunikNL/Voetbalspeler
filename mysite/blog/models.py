9766ab204a1f79d4eac9c090411bba9c1e85862d
YunikNL <Freddyvandenbergh@live.nl>
2023-10-01 19:45:01 +0200

    untracked files

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,16 +2,16 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Voetbalspeler(models.Model):
-        author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-        naam = models.CharField(max_length=100)
-        actuele_voetbalclub = models.CharField(max_length=100)
-        datum_invoer = models.DateTimeField(default=timezone.now)
-        datum_laaste_aanpassing = models.DateTimeField(blank=True, null=True)
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-        def opslaan(self):
-            self.datum_laaste_aanpassing = timezone.now()
-            self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-        def __str__(self):
-                return self.naam
+    def __str__(self):
+        return self.title
